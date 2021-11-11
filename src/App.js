9944d9ecea15a5458779726bf397133cc07fe089
SumonRayy <sumonrayy@gmail.com>
2021-11-11 11:14:37 +0530

    💤 appjs and indexjs updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  HttpLink, 
+  from } from '@apollo/client'
+import { onError } from '@apollo/client/link/error';
+import GetTodos from './components/GetTodos';
 
-function App() {
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {  // errorLink is a function for handling graphql errors
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+
+const link = from([   // link is an array of links
+  errorLink,
+  new HttpLink({
+    uri: process.env.REACT_APP_LOCAL_URI,
+  })
+]);
+
+
+const client = new ApolloClient({   // client is an instance of ApolloClient
+  cache: new InMemoryCache(),
+  link: link
+});
+
+function App() {  // App is a function that returns JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Apollo-React-Todo-App!</h1>
+        <GetTodos />
+      </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+export default App; // export App
