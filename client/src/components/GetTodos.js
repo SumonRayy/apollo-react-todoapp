8802a2946f63b6533d89987ef69905edf500fca9
SumonRayy <sumonrayy@gmail.com>
2021-11-12 17:33:25 +0530

    🐕‍🦺 Fetching Data Condtion modified with error handling

--- client/src/components/GetTodos.js
+++ client/src/components/GetTodos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {useQuery, gql} from '@apollo/client'
+import {useQuery} from '@apollo/client'
 import { GET_TODOS } from '../graphQL/Queries'
 import TodoCard from './TodoCard';
 import './TodoCard.css'
@@ -11,15 +11,22 @@ function GetTodos() {
 
     useEffect(() => {
         if(data){
+            if(loading){
+                console.log('loading');
+            }
             setTodos(data.getAllTodos);
+            if(error){
+                console.log(error);
+            }
         }
-    }, [data]);
+    }, [data, loading, error]);
 
     return (
         <div className="card-container">
             {todos.map(todo => (
                 <div key={todo.id}>
                     <TodoCard 
+                        id={todo.id}
                         title={todo.title}
                         detail={todo.detail}
                         date={todo.date}
