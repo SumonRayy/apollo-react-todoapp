5f01acca94e7ef96087c5c7914fdcd211dd743a4
SumonRayy <sumonrayy@gmail.com>
2021-11-12 17:35:14 +0530

    🧬 Mutations added : (ADD,DELETE,UPDATE)_TODO

---
+++ src/graphQL/Mutations.js
@@ -0,0 +1,25 @@
+import {gql} from '@apollo/client'
+
+export const ADD_TODO = gql`
+    mutation addTodo($title: String!, $detail: String!, $date: Date) {
+        addTodo(title: $title, detail: $detail, date: $date) {
+            id
+        }
+    }
+`
+
+export const DELETE_TODO = gql`
+    mutation deleteTodo($id: String!) {
+        deleteTodo(id: $id) {
+            id
+        }
+    }
+`
+
+export const UPDATE_TODO = gql`
+    mutation updateTodo($id: ID!, $title: String!, $detail: String!, $date: Date) {
+        updateTodo(id: $id, title: $title, detail: $detail, date: $date) {
+            id
+        }
+    }
+`
